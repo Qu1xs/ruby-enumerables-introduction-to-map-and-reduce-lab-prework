56bb4a733cc3b079631dea7d222c0fe697e89c0e
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-30 12:37:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,12 +43,9 @@ end
 def reduce_to_total(source_array, starting_point)
   i = 0
   sum = 0
-  if defined?(starting_point) && !starting_point.nil?
-  starting_point = starting_point
-  else
-  starting_point = 0
-  end
+  if defined?(starting_point)
   sum += starting_point
+  end
   while i < source_array.length do
     sum += source_array[i] 
     i += 1
