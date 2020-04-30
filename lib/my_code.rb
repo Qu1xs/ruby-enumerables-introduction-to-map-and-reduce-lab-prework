beb134a9577574f728eb227243c495cf594bea78
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-30 13:57:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,9 +68,9 @@ def reduce_to_any_true(source_array)
   i = 0
   while i < source_array.length do
     if !!source_array[i] 
-      new = false
-    else
       new = true
+    else
+      new = false
     end
     i += 1
   end
