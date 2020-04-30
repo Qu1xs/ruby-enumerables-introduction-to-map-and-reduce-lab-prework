7b6b9d995446fe815819994f6beed9bb13ae2fdb
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-30 11:50:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,6 +40,16 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
+  new = 0
+  i = 0
+  while i < source_array.length do
+    new += source_array[i] 
+    i += 1
+  end
+  return new
+end
+
+def reduce_to_all_true(source_array)
   new = []
   i = 0
   while i < source_array.length do
@@ -49,12 +59,14 @@ def reduce_to_total(source_array, starting_point)
   return new
 end
 
-def reduce_to_all_true(source_array)
-  
-end
-
 def reduce_to_any_true(source_array)
-  
+  new = 0
+  i = 0
+  while i < source_array.length do
+    new += source_array[i] 
+    i += 1
+  end
+  return new
 end
 
 
