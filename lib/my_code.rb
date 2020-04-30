cb4e3243bc315baefa3bc669c9b9b0dff52177ea
kevin murphy <kevinnealmurphy@gmail.com>
2020-04-30 13:51:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,6 +52,7 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
+  new = []
   i = 0
   while i < source_array.length do
     if !source_array[i]
@@ -59,6 +60,7 @@ def reduce_to_all_true(source_array)
     end
     i += 1
   end
+  return new
 end
 
 def reduce_to_any_true(source_array)
